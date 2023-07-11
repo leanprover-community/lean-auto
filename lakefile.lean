9e8bd39ace9f67233580f4aee262f89c62c46e9d
Jeremy Avigad <avigad@cmu.edu>
2023-07-11 16:43:09 -0400

    initialize lean4 project

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «auto» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «Auto» {
+  -- add any library configuration options here
+}
