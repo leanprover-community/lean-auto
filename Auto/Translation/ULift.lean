8500b0b9c19d6fe81a1df5c9514410ada902b444
IndPrinciple <syxx2013050321@163.com>
2023-07-29 11:42:16 -0400

    chore

--- Auto/Translation/ULift.lean
+++ Auto/Translation/ULift.lean
@@ -9,34 +9,42 @@ namespace Auto
 -- For an `e`, we denote its lifting as `t↑`. For the following
 --   discussion, we assume that we want to lift everything to
 --   universe level `u`.
--- `ULiftPos u e ty`:
+-- `CstULiftPos u e ty`:
 --   Given a type `ty` an expression `e : ty`, return
 --   (1) `e↑`
 --   (2) The type of `e↑`, i.e. `ty×`
--- `ULiftNeg u e↑ ty`:
+-- `CstULiftNeg u e↑ ty`:
 --   Given a type `ty` and an expression `e↑` where `e↑ : ty×`, return
 --   (1) `(e↑)↓`
 --   (2) The type of `e↑`, i.e. `ty×`.
---   Note that the type of `e↑` is unknown when we call `ULiftNeg`
+--   Note that the type of `e↑` is unknown when we call `CstULiftNeg`
 --   because there will be a free variable of unknown type acting
 --   as a hole inside `e↑`. This also explains why we need to
 --   return `ty×`.
 -- Note that `ULift` proceeds by structural recursion on
 --   `ty`, not on `e`.
--- **Make sure that** expressions should be β-reduced before being
---   sent to `ULiftPos` or `ULiftNeg`
+
+-- **Usage:** `CstULift` can be used to calculate ULift-ed version
+--   of [constants] and [free variables in the local context].
+-- Suppose we have a constant/free variable `A`. To calculate the
+--   ULift-ed version of `A` (denoted as `A↑`),
+--   (1) Compute the type of `A` (denoted as `ty`), instantiate
+--       metavariables in `ty` and β-reduce it to obtain `ty*`.
+--   (2) Let `u` be the universe level you want to lift to.
+--       Call `let A↑ ← CstULiftPos u A ty` to obtain the ULift-ed
+--       version of `A`
 
 mutual
 
   -- In the return type, the first `Expr` is `e↑`, and the second `Expr` is
   --   the type of `e↑`
-  partial def ULiftPos (u : Level) (e : Expr) : (ty : Expr) → MetaM (Expr × Expr)
+  partial def CstULiftPos (u : Level) (e : Expr) : (ty : Expr) → MetaM (Expr × Expr)
   | .bvar idx => throwError "Auto.ULift :: Loose bound variable"
   | .mvar mid => throwError "Auto.ULift :: Not implemented"
   | .lam .. => throwError "Auto.ULift :: Malformed type"
   | .letE .. => throwError "Auto.ULift :: Malformed type"
   | .lit .. => throwError "Auto.ULift :: Malformed type"
-  | .mdata data ty' => ULiftPos u e ty'
+  | .mdata data ty' => CstULiftPos u e ty'
   | .forallE name biTy body binfo => do
     -- We want to reate a free variable (intended) of type `bity↑`.
     --   However, we still don't what's `bity↑`, so we first
@@ -46,7 +54,7 @@ mutual
     -- `downFunc` is such that `downFunc binder↑` is equivalent to `binder`
     let (downFunc, biUpTy) ← Meta.withLocalDeclD `_ biUpTy? fun biUp => do
       -- To be safe, we call `instantiateMVars`
-      let (biUpDown, biUpTy) ← ULiftNeg u biUp (← instantiateMVars biTy)
+      let (biUpDown, biUpTy) ← CstULiftNeg u biUp (← instantiateMVars biTy)
       biUpTy?.mvarId!.assign biUpTy
       let downFunc ← Meta.mkLambdaFVars #[biUp] biUpDown
       return (downFunc, biUpTy)
@@ -56,7 +64,7 @@ mutual
       let eApp := Expr.app e biUpDown
       let eAppTy := Expr.instantiate1 body biUpDown
       -- `eAppTy` may contain `biUpTy?`, so we need `instantiateMVars`
-      let (eAppUp, eAppUpTy) ← ULiftPos u eApp (← instantiateMVars eAppTy)
+      let (eAppUp, eAppUpTy) ← CstULiftPos u eApp (← instantiateMVars eAppTy)
       let eUp ← Meta.mkLambdaFVars #[biUp] eAppUp
       let eUpTy ← Meta.mkForallFVars #[biUp] eAppUpTy
       let eUpTy ← instantiateMVars eUpTy
@@ -68,7 +76,7 @@ mutual
     --   3. `.app fn arg`
     --   4. `.fvar id`
     --
-    -- The same holds for `ULiftNeg`
+    -- The same holds for `CstULiftNeg`
     let sort ← instantiateMVars (← Meta.inferType ty)
     if !sort.isSort then
       throwError "Auto.ULift :: Expected sort"
@@ -79,23 +87,23 @@ mutual
 
   -- In the return type, the first `Expr` is `eUp↓`, and the second `Expr` is
   --   the type of `e↑`
-  partial def ULiftNeg (u : Level) (eUp : Expr) : (ty : Expr) → MetaM (Expr × Expr)
+  partial def CstULiftNeg (u : Level) (eUp : Expr) : (ty : Expr) → MetaM (Expr × Expr)
   | .bvar idx => throwError "Auto.ULift :: Loose bound variable"
   | .mvar mid => throwError "Auto.ULift :: Not implemented"
   | .lam .. => throwError "Auto.ULift :: Malformed type"
   | .letE .. => throwError "Auto.ULift :: Malformed type"
   | .lit .. => throwError "Auto.ULift :: Malformed type"
-  | .mdata data ty' => ULiftNeg u eUp ty'
+  | .mdata data ty' => CstULiftNeg u eUp ty'
   | .forallE name biTy body binfo => do
     -- `upFunc` is such that `upFunc binder` is equivalent to `binder↑`
     let (upFunc, biUpTy) ← Meta.withLocalDeclD `_ biTy fun bi => do
       -- To be safe, we call `instantiateMVars`
-      let (biUp, biUpTy) ← ULiftPos u bi (← instantiateMVars biTy)
+      let (biUp, biUpTy) ← CstULiftPos u bi (← instantiateMVars biTy)
       let upFunc ← Meta.mkLambdaFVars #[bi] biUp
       return (upFunc, biUpTy)
     -- `downFunc` is such that `downFunc binder↑` is equivalent to `binder`
     let downFunc ← Meta.withLocalDeclD `_ biUpTy fun biUp => do
-      let (biUpDown, _) ← ULiftNeg u biUp (← instantiateMVars biTy)
+      let (biUpDown, _) ← CstULiftNeg u biUp (← instantiateMVars biTy)
       Meta.mkLambdaFVars #[biUp] biUpDown
     let (e, eUpTyPre) ← Meta.withLocalDecl name binfo biTy fun bi => do
       -- `eUpApp = eUp (upFunc bi)`
@@ -103,7 +111,7 @@ mutual
       let eUpApp := Expr.app eUp biUp
       let eAppTy := Expr.instantiate1 body bi
       -- To be safe, we call `instantiateMVars`
-      let (eApp, eAppUpTy) ← ULiftNeg u eUpApp (← instantiateMVars eAppTy)
+      let (eApp, eAppUpTy) ← CstULiftNeg u eUpApp (← instantiateMVars eAppTy)
       let e ← Meta.mkLambdaFVars #[bi] eApp
       let eUpTyPre ← Meta.mkForallFVars #[bi] eAppUpTy
       return (e, eUpTyPre)
@@ -123,13 +131,25 @@ mutual
 
 end
 
-section Test
+-- Postprocess expression `e` obtained from ULifting
+def postProcessULift (e : Expr) : MetaM Expr := do
+  let e ← Core.betaReduce e
+  let red (e : Expr) : CoreM TransformStep := do
+    match e with
+    | .app (.app (.const ``GLift.up lvl₁) ty₁) (.app (.app (.const ``GLift.down lvl₂) ty₂) e') =>
+      return .continue e'
+    | _ => return .continue e
+  liftM <| (Core.transform (pre := red) e : CoreM _)
+
+section TestCstULift
 
   universe tmp
 
   private def ulift (e : Expr) : Elab.TermElabM Unit := do
     let ety ← Meta.inferType e
-    let (eup, eupTy) ← ULiftPos (.param `tmp) e ety
+    let ety ← Core.betaReduce ety
+    let (eup, eupTy) ← CstULiftPos (.param `tmp) e ety
+    let eup ← postProcessULift eup
     logInfo eup
   
   set_option pp.universes true
@@ -169,30 +189,23 @@ section Test
   #getExprAndApply[@Nat.rec | ulift]
   universe u_1
   #check fun {motive : GLift.{1, tmp} Nat → GLift.{u_1 + 1, tmp} (Sort u_1)}
-    (zero :
-      GLift.{u_1, tmp}
-        ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t))) Nat.zero))
+    (zero : GLift.{u_1, tmp} (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat Nat.zero))))
     (succ :
       (n : GLift.{1, tmp} Nat) →
         GLift.{u_1, tmp}
-            ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t)))
-              (@GLift.down.{1, tmp} Nat n)) →
+            (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat (@GLift.down.{1, tmp} Nat n)))) →
           GLift.{u_1, tmp}
-            ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t)))
-              (Nat.succ (@GLift.down.{1, tmp} Nat n))))
+            (@GLift.down.{u_1 + 1, tmp} (Sort u_1)
+              (motive (@GLift.up.{1, tmp} Nat (Nat.succ (@GLift.down.{1, tmp} Nat n))))))
     (t : GLift.{1, tmp} Nat) =>
   @GLift.up.{u_1, tmp}
-    ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t)))
-      (@GLift.down.{1, tmp} Nat t))
+    (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat (@GLift.down.{1, tmp} Nat t))))
     (@Nat.rec.{u_1} (fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t)))
-      (@GLift.down.{u_1, tmp}
-        ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t))) Nat.zero) zero)
+      (@GLift.down.{u_1, tmp} (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat Nat.zero))) zero)
       (fun (n : Nat) (n_ih : @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat n))) =>
-        @GLift.down.{u_1, tmp}
-          ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t))) (Nat.succ n))
+        @GLift.down.{u_1, tmp} (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat (Nat.succ n))))
           (succ (@GLift.up.{1, tmp} Nat n)
-            (@GLift.up.{u_1, tmp}
-              ((fun (t : Nat) => @GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat t))) n) n_ih)))
+            (@GLift.up.{u_1, tmp} (@GLift.down.{u_1 + 1, tmp} (Sort u_1) (motive (@GLift.up.{1, tmp} Nat n))) n_ih)))
       (@GLift.down.{1, tmp} Nat t))
 
   private axiom f₆ : ∀ (α : Nat → Type) (β : ∀ (f : Nat → Nat), α (f 0)), Nat
@@ -200,19 +213,11 @@ section Test
   #check fun (α : GLift.{1, tmp} Nat → GLift.{2, tmp} Type)
     (β :
       (f : GLift.{1, tmp} Nat → GLift.{1, tmp} Nat) →
-        GLift.{1, tmp}
-          ((fun (a : Nat) => GLift.down.{2, tmp} (α (GLift.up.{1, tmp} a)))
-            ((fun (a : Nat) => GLift.down.{1, tmp} (f (GLift.up.{1, tmp} a))) 0))) =>
+        GLift.{1, tmp} (GLift.down.{2, tmp} (α (f (GLift.up.{1, tmp} 0))))) =>
   GLift.up.{1, tmp}
     (Auto.f₆ (fun (a : Nat) => GLift.down.{2, tmp} (α (GLift.up.{1, tmp} a))) fun (f : Nat → Nat) =>
       GLift.down.{1, tmp} (β fun (a : GLift.{1, tmp} Nat) => GLift.up.{1, tmp} (f (GLift.down.{1, tmp} a))))
-  #reduce (α : GLift.{1, tmp} Nat → GLift.{2, tmp} Type) →
-  ((f : GLift.{1, tmp} Nat → GLift.{1, tmp} Nat) →
-      GLift.{1, tmp}
-        ((fun (a : Nat) => GLift.down.{2, tmp} (α (GLift.up.{1, tmp} a)))
-          ((fun (a : Nat) => GLift.down.{1, tmp} (f (GLift.up.{1, tmp} a))) 0))) →
-    GLift.{1, tmp} Nat
 
-end Test
+end TestCstULift
 
 end Auto
