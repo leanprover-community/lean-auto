a6f49c00f84b8fedd3ed9ac96afeb76747c980cf
IndPrinciple <syxx2013050321@163.com>
2023-10-12 14:38:44 +0800

    works on Propositional, works?? on ring

--- Examples/Propositional.lean
+++ Examples/Propositional.lean
@@ -3,38 +3,39 @@ Examples in propositional logic.
 -/
 
 -- From TPIL
+import Auto.Tactic
 
 section
 variable (P Q R S : Prop)
 
-example :P ∧ Q ↔ Q ∧ P := by sorry
-example :P ∨ Q ↔ Q ∨ P := by sorry
-example : (P ∧ Q) ∧ R ↔ P ∧ (Q ∧ R) := by sorry
-example : (P ∨ Q) ∨ R ↔ P ∨ (Q ∨ R) := by sorry
-example : P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := by sorry
-example : P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (P ∨ R) := by sorry
-example : (P → (Q → R)) ↔ (P ∧ Q → R) := by sorry
-example : ((P ∨ Q) → R) ↔ (P → R) ∧ (Q → R) := by sorry
-example : ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := by sorry
-example : ¬P ∨ ¬Q → ¬(P ∧ Q) := by sorry
-example : ¬(P ∧ ¬P) := by sorry
-example : P ∧ ¬Q → ¬(P → Q) := by sorry
-example : ¬P → (P → Q) := by sorry
-example : (¬P ∨ Q) → (P → Q) := by sorry
-example : P ∨ False ↔ P := by sorry
-example : P ∧ False ↔ False := by sorry
-example : ¬(P ↔ ¬P) := by sorry
-example : (P → Q) → (¬Q → ¬P) := by sorry
+example :P ∧ Q ↔ Q ∧ P := by auto
+example :P ∨ Q ↔ Q ∨ P := by auto
+example : (P ∧ Q) ∧ R ↔ P ∧ (Q ∧ R) := by auto
+example : (P ∨ Q) ∨ R ↔ P ∨ (Q ∨ R) := by auto
+example : P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := by auto
+example : P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (P ∨ R) := by auto
+example : (P → (Q → R)) ↔ (P ∧ Q → R) := by auto
+example : ((P ∨ Q) → R) ↔ (P → R) ∧ (Q → R) := by auto
+example : ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := by auto
+example : ¬P ∨ ¬Q → ¬(P ∧ Q) := by auto
+example : ¬(P ∧ ¬P) := by auto
+example : P ∧ ¬Q → ¬(P → Q) := by auto
+example : ¬P → (P → Q) := by auto
+example : (¬P ∨ Q) → (P → Q) := by auto
+example : P ∨ False ↔ P := by auto
+example : P ∧ False ↔ False := by auto
+example : ¬(P ↔ ¬P) := by auto
+example : (P → Q) → (¬Q → ¬P) := by auto
 
 -- classical
 
-example : (P → R ∨ S) → ((P → R) ∨ (P → S)) := by sorry
-example : ¬(P ∧ Q) → ¬P ∨ ¬Q := by sorry
-example : ¬(P → Q) → P ∧ ¬Q := by sorry
-example : (P → Q) → (¬P ∨ Q) := by sorry
-example : (¬Q → ¬P) → (P → Q) := by sorry
-example : P ∨ ¬P := by sorry
-example : (((P → Q) → P) → P) := by sorry
+example : (P → R ∨ S) → ((P → R) ∨ (P → S)) := by auto
+example : ¬(P ∧ Q) → ¬P ∨ ¬Q := by auto
+example : ¬(P → Q) → P ∧ ¬Q := by auto
+example : (P → Q) → (¬P ∨ Q) := by auto
+example : (¬Q → ¬P) → (P → Q) := by auto
+example : P ∨ ¬P := by auto
+example : (((P → Q) → P) → P) := by auto
 
 end
 
@@ -43,13 +44,13 @@ end
 section
 variable (A B C : Prop)
 
-example (h : A) (h₁ : ¬ B) : ¬ (A → B) := by sorry
-example : ¬ (A ↔ ¬ A) := by sorry
-example (h : A ↔ B) (h₁ : A ∧ B → C) (h₂ : ¬ A ∧ ¬ B → C) : C := by sorry
-example (h : A ↔ B) (h₁ : (A ∧ ¬ B) ∨ (¬ A ∧ B)) : C := by sorry
-example (h : A → B) (h₁ : A) : B := by sorry
-example (h : A → B) (h₁ : B → C) : A → C := by sorry
-example (h : A → B ∨ C) (h₁ : B → D) (h₂ : C → D) : A → D := by sorry
+example (h : A) (h₁ : ¬ B) : ¬ (A → B) := by auto
+example : ¬ (A ↔ ¬ A) := by auto
+example (h : A ↔ B) (h₁ : A ∧ B → C) (h₂ : ¬ A ∧ ¬ B → C) : C := by auto
+example (h : A ↔ B) (h₁ : (A ∧ ¬ B) ∨ (¬ A ∧ B)) : C := by auto
+example (h : A → B) (h₁ : A) : B := by auto
+example (h : A → B) (h₁ : B → C) : A → C := by auto
+example (h : A → B ∨ C) (h₁ : B → D) (h₂ : C → D) : A → D := by auto
 
 end
 
@@ -59,71 +60,71 @@ section
 variable (A₁ A₂ A₃ A₄ B₁ B₂ B₃ B₄ : Prop)
 
 example (H1 : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) : B₄ := by sorry
+  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) : B₄ := by auto
 example (H1 : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₄) : B₃ := by sorry
+  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₄) : B₃ := by auto
 example (H1 : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n3 : ¬B₃) (n3 : ¬B₄) : B₂ := by sorry
+  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n3 : ¬B₃) (n3 : ¬B₄) : B₂ := by auto
 example (H1 : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : B₁ := by sorry
+  (a1 : A₁) (a2 : A₂) (a3 : A₃) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : B₁ := by auto
 
 example (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a2 : A₂) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₃ := by sorry
+  (a1 : A₁) (a2 : A₂) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₃ := by auto
 example (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a1 : A₁) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₂ := by sorry
+  (a1 : A₁) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₂ := by auto
 example (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄)
-  (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₁ := by sorry
+  (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄) : ¬A₁ := by auto
 
 -- H last, all pos
 example (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₄ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₄ := by auto
 example (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₃ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₃ := by auto
 example (a1 : A₁) (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n3 : ¬B₃) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₂ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₂ := by auto
 example (a1 : A₁) (a2 : A₂) (a3 : A₃) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₁ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : B₁ := by auto
 
 example (a1 : A₁) (a2 : A₂) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₃ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₃ := by auto
 example (a1 : A₁) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₂ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₂ := by auto
 example (a2 : A₂) (a3 : A₃) (n1 : ¬B₁) (n2 : ¬B₂) (n3 : ¬B₃) (n3 : ¬B₄)
-  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₁ := by sorry
+  (H : A₁ → A₂ → A₃ → B₁ ∨ B₂ ∨ B₃ ∨ B₄) : ¬A₁ := by auto
 
 -- H first, all neg
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) : ¬B₄ := by sorry
+  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) : ¬B₄ := by auto
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b4 : B₄) : ¬B₃ := by sorry
+  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b4 : B₄) : ¬B₃ := by auto
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b3 : B₃) (b4 : B₄) : ¬B₂ := by sorry
+  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b3 : B₃) (b4 : B₄) : ¬B₂ := by auto
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬B₁ := by sorry
+  (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬B₁ := by auto
 
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n2 : ¬A₂) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₃ := by sorry
+  (n1 : ¬A₁) (n2 : ¬A₂) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₃ := by auto
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n1 : ¬A₁) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₂ := by sorry
+  (n1 : ¬A₁) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₂ := by auto
 example (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄)
-  (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₁ := by sorry
+  (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄) : ¬¬A₁ := by auto
 
 -- H last, all neg
 example (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₄ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₄ := by auto
 example (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₃ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₃ := by auto
 example (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b3 : B₃) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₂ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₂ := by auto
 example (n1 : ¬A₁) (n2 : ¬A₂) (n3 : ¬A₃) (b2 : B₂) (b3 : B₃) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₁ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬B₁ := by auto
 
 example (n1 : ¬A₁) (n2 : ¬A₂) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₃ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₃ := by auto
 example (n1 : ¬A₁) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₂ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₂ := by auto
 example (n2 : ¬A₂) (n3 : ¬A₃) (b1 : B₁) (b2 : B₂) (b3 : B₃) (b4 : B₄)
-  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₁ := by sorry
+  (H : ¬A₁ → ¬A₂ → ¬A₃ → ¬B₁ ∨ ¬B₂ ∨ ¬B₃ ∨ ¬B₄) : ¬¬A₁ := by auto
 
 end
 
@@ -132,5 +133,5 @@ variable (Scottish RedSocks WearKilt Married GoOutSunday : Prop)
 
 theorem NoMember : (¬Scottish → RedSocks) → (WearKilt ∨ ¬RedSocks) → (Married → ¬GoOutSunday) →
                  (GoOutSunday ↔ Scottish) → (WearKilt → Scottish ∧ Married) →
-                 (Scottish → WearKilt) → False := by sorry
+                 (Scottish → WearKilt) → False := by auto
 end club
--- Examples/Ring.lean
+++ Examples/Ring.lean
@@ -1,9 +1,22 @@
 import Mathlib.Tactic.Ring
+import Auto.Tactic
+
+set_option trace.auto.tptp.result true
+set_option tptp.solver.name "zeport"
+set_option tptp.zeport.path "/home/indprinciple/Programs/zipperposition/portfolio/portfolio.fo.parallel.py"
 
 example (x y z : Int) : (x + y + z)^2 = x^2 + y^2 + z^2 + 2*x*y + 2*x*z + 2*y*z := by
   ring
+
+example (x y z : Int) : (x + y + z)^2 = x^2 + y^2 + z^2 + 2*x*y + 2*x*z + 2*y*z := by
+  simp [two_mul, pow_two]
+  auto [add_mul, mul_add, add_comm, mul_comm, add_assoc]
 
 variable {R : Type _} [CommRing R]
 
 example (x y z : R) : (x + y + z)^2 = x^2 + y^2 + z^2 + 2*x*y + 2*x*z + 2*y*z := by
   ring
+
+example (x y z : R) : (x + y + z)^2 = x^2 + y^2 + z^2 + 2*x*y + 2*x*z + 2*y*z := by
+  simp [two_mul, pow_two]
+  auto [add_mul, mul_add, add_comm, mul_comm, add_assoc]
