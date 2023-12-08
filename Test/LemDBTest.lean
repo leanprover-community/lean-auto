27235eea1adfda0aa2267cdc9a1926e90c3ff1f8
IndPrinciple <syxx2013050321@163.com>
2023-12-08 12:08:31 +0800

    update

--- Test/LemDBTest.lean
+++ Test/LemDBTest.lean
@@ -1,5 +1,5 @@
 import Auto.Tactic
-import Std.Data.BitVec
+
 set_option auto.smt true
 set_option auto.smt.trust true
 set_option trace.auto.smt.printCommands true
@@ -19,12 +19,12 @@ def Zone.MinArea1 : Zone → Area
   | .Z3 => 3500
   | .Z4 => 2500
 
-theorem Zone.MinArea1.defeq :
+theorem Zone.MinArea1.spec :
   Zone.MinArea1 .Z1 = 10000 ∧ Zone.MinArea1 .Z2 = 5000 ∧
   Zone.MinArea1 .Z3 = 3500 ∧ Zone.MinArea1 .Z4 = 2500 := by simp
 
 example (x : Zone) : x.MinArea1 >= 2500 := by cases x <;> simp -- succeeds
-example (x : Zone) : x.MinArea1 >= 2500 := by auto [Zone.MinArea1.defeq]
+example (x : Zone) : x.MinArea1 >= 2500 := by auto [Zone.MinArea1.spec]
 
 def Zone.MinArea2 : Zone → Area
   | .Z1 => 0
@@ -32,21 +32,45 @@ def Zone.MinArea2 : Zone → Area
   | .Z3 => 6500
   | .Z4 => 7500
 
-theorem Zone.MinArea2.defeq :
+theorem Zone.MinArea2.spec :
   Zone.MinArea2 .Z1 = 0 ∧ Zone.MinArea2 .Z2 = 5000 ∧
   Zone.MinArea2 .Z3 = 6500 ∧ Zone.MinArea2 .Z4 = 7500 := by simp
 
 example (x : Zone) : x.MinArea1 + x.MinArea2 = 10000 := by
-  auto [Zone.MinArea1.defeq, Zone.MinArea2.defeq]
+  auto [Zone.MinArea1.spec, Zone.MinArea2.spec]
 example (x : Zone) (_ : x = .Z1) : x.MinArea1 = 10000 && x.MinArea2 = 0 := by
-  auto [*, Zone.MinArea1.defeq, Zone.MinArea2.defeq]
+  auto [*, Zone.MinArea1.spec, Zone.MinArea2.spec]
 
-#declare_lemdb zone_defeq
+#declare_lemdb zone_spec
 open Zone in
-attribute [lemdb zone_defeq] MinArea1.defeq MinArea2.defeq
+attribute [lemdb zone_spec] MinArea1.spec MinArea2.spec
 
 example (x : Zone) : x.MinArea1 + x.MinArea2 = 10000 := by
-  auto [*zone_defeq]
+  auto [*zone_spec]
 
 example (x : Zone) : x.MinArea1 >= 2500 && x.MinArea2 <= 7500 := by
-  auto [*zone_defeq]
+  auto [*zone_spec]
+
+def Nat.op1 : Nat → Nat
+| 0 => 10
+| _ + 1 => 2
+
+theorem Nat.op1.spec : Nat.op1 0 = 10 ∧ Nat.op1 (.succ n) = 2 := And.intro rfl rfl
+
+def Nat.op2 (n : Nat) : Nat :=
+  if n < 10 then 10 else n
+
+theorem Nat.op2.spec : (n < 10 → Nat.op2 n = 10) ∧ (n >= 10 → Nat.op2 n = n) := by
+  apply And.intro <;> intro h <;> unfold op2
+  case left => simp [h]
+  case right => simp [Nat.not_lt_of_le h]
+
+#declare_lemdb op_spec
+attribute [lemdb op_spec] Nat.op1.spec Nat.op2.spec
+
+example : Nat.op1 n >= 2 ∧ Nat.op2 n >= 10 := by
+  auto [*op_spec]
+
+#compose_lemdb all_spec [zone_spec, op_spec]
+example (x : Zone) : x.MinArea1 + x.MinArea2 = 10000 ∧ Nat.op1 n >= 2 ∧ Nat.op2 n >= 10 := by
+  auto [*all_spec]
