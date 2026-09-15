92d628ba41801bd12c9d81f9be12fde97f0ad177
JOSHCLUNE <josh.seth.clune@gmail.com>
2026-09-15 16:21:26 -0400

    chore: Update to v4.34.0

--- Auto/Lib/BoolExtra.lean
+++ Auto/Lib/BoolExtra.lean
@@ -99,8 +99,8 @@ theorem Bool.ite'_eq_false (p : Prop) (a b : α) : ¬ p → ite' p a b = b := by
 -/
 theorem Bool.cond_simp.{u} : @cond = fun (α : Sort u) b => @ite' α (b = true) := by
   funext α b x y; cases b
-  case false => rw [cond_false, Bool.ite'_eq_false]; exact Bool.false_ne_true
-  case true => rw [cond_true, Bool.ite'_eq_true]; rfl
+  case false => rw [Bool.cond_false, Bool.ite'_eq_false]; exact Bool.false_ne_true
+  case true => rw [Bool.cond_true, Bool.ite'_eq_true]; rfl
 
 /--
   Invoke `rw` at the beginning of `auto`
